b3ff1b710b49274be3d8e8c77df0eac8ffe54ecd
adityabhartib <adityabharti.ab@gmail.com>
2022-10-06 14:25:26 +0530

    Swap two Number Addded

---
+++ src/com/bridgelabz/basicCoreProgram/SwapTwoNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.basicCoreProgram;
+
+import java.util.Scanner;
+
+public class SwapTwoNumber {
+    static void swapNumber(int num1, int num2){
+        int temp;
+        System.out.println("Before Swaping: "+num1+","+num2);
+        temp=num1; //num1=num1+num2;
+        num1=num2; //num2=num1-num2;
+        num2=temp; //num1=num1-num2;
+        System.out.println("After Swaping: "+num1+","+num2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Two Number: ");
+        int num1= sc.nextInt();
+        int num2=sc.nextInt();
+        swapNumber(num1,num2);
+    }
+}
