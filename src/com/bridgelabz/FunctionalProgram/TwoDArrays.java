8763cf7887ed64d24a3232b59df8ec15d378851e
adityabhartib <adityabharti.ab@gmail.com>
2022-10-08 11:38:04 +0530

    Logical Program Added

--- src/com/bridgelabz/FunctionalProgram/TwoDArrays.java
+++ src/com/bridgelabz/FunctionalProgram/TwoDArrays.java
@@ -1,15 +1,25 @@
 package com.bridgelabz.FunctionalProgram;
 
-public class TwoDArrays {
-    public static void main(String[] args) {
-        int[][] array2 = new int[4][5];
+import java.util.Scanner;
 
-        for (int i = 0; i < array2.length;i++){
-            for (int j=0;j<array2[i].length;j++){
-                array2[i][j]=i+j;
-                System.out.print(array2[i][j]+ " ");
+public class TwoDArrays {
+    static  void print2DArray(int m,int n) {
+        int[][] array2 = new int[m][n];
+
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                array2[i][j] = i + j;
+                System.out.print(array2[i][j] + " ");
             }
-            System.out.println( );
+            System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Row: ");
+        int m= sc.nextInt();
+        System.out.println("Enter Collum:" );
+        int n= sc.nextInt();
+        print2DArray(m,n);
     }
 }
