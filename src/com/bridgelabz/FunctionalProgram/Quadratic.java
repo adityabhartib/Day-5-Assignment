72d2854c6fe9034ed1820b1bf09f59980bbe4153
adityabhartib <adityabharti.ab@gmail.com>
2022-10-15 10:06:44 +0530

    Quadratic Added

--- src/com/bridgelabz/FunctionalProgram/Quadratic.java
+++ src/com/bridgelabz/FunctionalProgram/Quadratic.java
@@ -12,19 +12,16 @@ public class Quadratic {
         System.out.print("Enter the value of c: ");
         double c = input.nextDouble();
         double d= b * b - 4.0 * a * c;
-        if (d> 0.0)
-        {
+        if (d> 0.0) {
             double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
             double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
             System.out.println("The roots are " + r1 + " and " + r2);
         }
-        else if (d == 0.0)
-        {
+        else if (d == 0.0) {
             double r1 = -b / (2.0 * a);
             System.out.println("The root is " + r1);
         }
-        else
-        {
+        else {
             System.out.println("Roots are not real.");
         }
     }
